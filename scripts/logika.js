8cc42ac476aa313f44e6ac75aa08aa35bc68ad12
Grigoriy-iv <ev.grishkavec@gmail.com>
2020-06-09 22:18:19 +0300

    09.06.2020

--- scripts/logika.js
+++ scripts/logika.js
@@ -1,25 +1,29 @@
-//          При нажатии на карточку, она переворачивается
-
 let cards = document.querySelectorAll(".card");
+let openedOneCard;
+
 (function mix() {
     for (let i = 0; i < cards.length; i++) {
         let card = cards[i];
         card.style.order = Math.floor(Math.random() * 20);
-        card.addEventListener("click", rotate);
-            function rotate() {
-                card.classList.add("rotate");
-            }
     }
-})()
+})();
 
-//                     Массив из случайных чисел --> Math.floor
-
-// (function mix() {
-//     for (let i = 0; i < cards.length; i++) {
-//         let card = cards[i];
-//         card.style.order = Math.floor(Math.random() * 20);
-//     }
-// })();
-
-//                      Проверяем на совпадение
-
+for (let i = 0; i < cards.length; i++) {
+    let card = cards[i];
+    card.addEventListener("click", rotate);
+    function rotate() {
+        card.classList.add("rotate");
+        if (!openedOneCard) {
+            openedOneCard = card;
+        }
+        else {
+            setTimeout(() => {
+                if (openedOneCard.className !== card.className) {
+                    openedOneCard.classList.remove("rotate");
+                    card.classList.remove("rotate");
+                }
+                openedOneCard = null;
+            }, 1300);
+        }
+    }
+}
