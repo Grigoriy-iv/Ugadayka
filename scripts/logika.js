058b7a0a993e4cfd501a84b4188a7dc518c400a8
Grigoriy-iv <ev.grishkavec@gmail.com>
2020-06-29 21:06:06 +0300

    29.06.2020

--- scripts/logika.js
+++ scripts/logika.js
@@ -1,6 +1,12 @@
 let cards = document.querySelectorAll(".card");
+let btn_play = document.querySelector(".btn_play");
+btn_play.addEventListener("click", startGame);
+let time = document.querySelector(".time");
 let openedOneCard;
 let block = false;
+let seconds = 0;
+let minutes = 0;
+let lock_time = false;
 
 (function mix() {
     for (let i = 0; i < cards.length; i++) {
@@ -9,30 +15,67 @@ let block = false;
     }
 })();
 
-for (let i = 0; i < cards.length; i++) {
-    let card = cards[i];
-    card.addEventListener("click", rotate);
-    function rotate() {
-        if (block === true) {
-            return;
+function startGame() {
+    setTimeout(() => {
+        if (lock_time === false) {
+            lock_time = true;
+            setInterval(timer, 1000);
         }
-        card.classList.add("rotate");
-        if (!openedOneCard) {
-            openedOneCard = card;
+        for (let i = 0; i < cards.length; i++) {
+            let card = cards[i];
+            card.addEventListener("click", rotate);
+            function rotate() {
+                if (block === true) {
+                    return;
+                }
+                card.classList.add("rotate");
+                if (!openedOneCard) {
+                    openedOneCard = card;
+                }
+                else {
+                    if (openedOneCard === card) {
+                        return;
+                    }
+                    block = true;
+                    setTimeout(() => {
+                        if (openedOneCard.className !== card.className) {
+                            openedOneCard.classList.remove("rotate");
+                            card.classList.remove("rotate");
+                        }
+                        openedOneCard = null;
+                        block = false;
+                    }, 1300);
+                }
+            }
+        }
+    }, 3000);
+}
+
+function timer() {
+    seconds ++;
+    if (seconds >= 60) {
+        minutes++;
+        seconds = 0;
+    }
+    if (seconds < 10) {
+        if (minutes < 10) {
+            time.innerHTML = "0" + minutes + ":0" + seconds;
         }
         else {
-            if (openedOneCard === card) {
-                return;
-            }
-            block = true;
-            setTimeout(() => {
-                if (openedOneCard.className !== card.className) {
-                    openedOneCard.classList.remove("rotate");
-                    card.classList.remove("rotate");
-                }
-                openedOneCard = null;
-                block = false;
-            }, 1300);
+            time.innerHTML = "" + minutes + ":0" + seconds;
+        }
+    }
+    else {
+        if (minutes < 10) {
+            time.innerHTML = "0" + minutes + ":" + seconds;
+        }
+        else {
+            time.innerHTML = "" + minutes + ":" + seconds;
         }
     }
 }
+
+
+// function stopGame() {
+//     if
+// }
